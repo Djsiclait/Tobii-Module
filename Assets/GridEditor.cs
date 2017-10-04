794a645cb79f70cfe9f68756b15f3f2c458ec8c7
Djsiclait <djidjellypas92@gmail.com>
2017-10-04 15:12:55 -0400

    Changes

--- Assets/GridEditor.cs
+++ Assets/GridEditor.cs
@@ -4,6 +4,8 @@ using UnityEngine.UI;
 using System.IO;
 using System;
 using Tobii.Gaming;
+using System.Collections.Generic;
+
 public class GridEditor : MonoBehaviour {
 
     private int xcounter = 0;
@@ -17,10 +19,15 @@ public class GridEditor : MonoBehaviour {
     public GameObject word;
     public Button nextPage;
     private ArrayList words;
+    private List<ArrayList> pages;
+    private int currentpage;
     private static int maxRowlength = 80;
+    private static int maxLineLength = 5;
+    private int numPages;
     // Use this for initialization
     void Start () {
         words = new ArrayList();
+        pages = new List<ArrayList>();
         maxRowlength = 80;
         try
         {
@@ -38,6 +45,24 @@ public class GridEditor : MonoBehaviour {
                 }
             }
 
+            numPages = words.Count / (maxRowlength * maxLineLength);
+            print(words.Count);
+            print(numPages);
+
+            for (int i = 0; i < numPages; i++)
+            {
+                ArrayList buffer = new ArrayList();
+
+                for (int j = (maxRowlength * maxLineLength) * i; j < (maxRowlength * maxLineLength) * (i + 1); j++)
+                {
+                    while (j < words.Count)
+                        buffer.Add(words[j]);
+                }
+
+                pages.Add(buffer);
+            }
+            currentpage = 0;
+
         }
         catch (Exception e)
         {
@@ -46,6 +71,7 @@ public class GridEditor : MonoBehaviour {
             Console.WriteLine("The file could not be read:");
             Console.WriteLine(e.Message);
         }
+
         firstrow = Instantiate(row) as GameObject;
         //newText.text = string.Format("Item {0}", counter.ToString());
         firstrow.transform.parent = page.transform;
@@ -66,34 +92,61 @@ public class GridEditor : MonoBehaviour {
         }
     }
 
+    public void GoBack()
+    {
+        AddItemToGrid();
+    }
+
     public void AddItemToGrid()
     {
+        foreach (Transform child in page.transform)
+            child.parent = null;
+
         GameObject currentRow = firstrow;
-        while (xcounter < words.Count && ycounter < pageend) {
-            if (currentRowLength < maxRowlength)
+
+        ArrayList data = new ArrayList();
+        int counter = 0;
+
+        foreach (ArrayList a in pages)
+            if (counter == currentpage)
+                data = a;
+            else
+                counter++;
+
+        xcounter = 0;
+        ycounter = 0;
+
+            while (xcounter < data.Count && ycounter < pageend)
             {
-                currentRowLength += words[xcounter].ToString().Length;
-                GameObject newText = Instantiate(word);
-                TextMesh temp = newText.GetComponent(typeof(TextMesh)) as TextMesh;
-                BoxCollider boxCollider = newText.GetComponent(typeof(BoxCollider)) as BoxCollider;
-                temp.text = words[xcounter].ToString();
-                temp.characterSize = 25;
-                print(temp.text);
-                newText.AddComponent<BoxCollider>();
-                newText.name = temp.text;
-                //newText.text = string.Format("Item {0}", counter.ToString());
-                newText.transform.parent = currentRow.transform;
+                if (currentRowLength < maxRowlength)
+                {
+                    currentRowLength += data[xcounter].ToString().Length;
+                    GameObject newText = Instantiate(word);
+                    TextMesh temp = newText.GetComponent(typeof(TextMesh)) as TextMesh;
+                    BoxCollider boxCollider = newText.GetComponent(typeof(BoxCollider)) as BoxCollider;
+                    temp.text = data[xcounter].ToString();
+                    temp.characterSize = 25;
+                    print(temp.text);
+                    newText.AddComponent<BoxCollider>();
+                    newText.name = temp.text;
+                    //newText.text = string.Format("Item {0}", counter.ToString());
+                    newText.transform.parent = currentRow.transform;
+                }
+                else
+                {
+                    currentRowLength = 0;
+                    var newrow = Instantiate(row) as GameObject;
+                    ycounter++;
+                    //newText.text = string.Format("Item {0}", counter.ToString());
+                    currentRow = newrow;
+                    currentRow.transform.parent = page.transform;
+                }
+                xcounter++;
             }
-            else {
-                currentRowLength = 0;
-                var newrow = Instantiate(row) as GameObject;
-                ycounter++;
-                //newText.text = string.Format("Item {0}", counter.ToString());
-                currentRow = newrow;
-                currentRow.transform.parent = page.transform;
-            }
-            xcounter++;
-        }
-       
+
+        if (currentpage + 1 < numPages)
+            currentpage++;
+        else
+            currentpage = 0;
     }
 }
